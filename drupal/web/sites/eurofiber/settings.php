ad77e531253b9ed32d7224ba39c94f10ab3a970a
Perry Janssen <perry@burst-digital.com>
2021-02-09 17:16:31 +0100

    feat(drupal): Add a new site and add multilanguage modules and taxonomy

---
+++ drupal/web/sites/eurofiber/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/../default/settings.php';
+
+$databases['default']['default']['database'] = 'eurofiber';
---
+++ drupal/web/sites/sites.php
@@ -0,0 +1,36 @@
+<?php
+
+// @codingStandardsIgnoreFile
+
+$base_sites = [
+  'cms.eurofiber.com' => 'eurofiber',
+];
+
+global $sites;
+
+foreach ($base_sites as $domain => $folder) {
+  $sites[$domain] = $folder;
+
+  /**
+   * We also want to make it work with local development. For this, we need to
+   * add .localhost after all domains.
+   *
+   * Unfortunately, Lando doesn't support multiple databases in the same service,
+   * so for now we will always just redirect to the default site.
+   */
+  $sites[$domain . '.localhost'] = 'default';
+
+  /**
+   * A platform URL can look like this:
+   * https://cms.example.com.master-XXXXXXX-XXXXXXXXXXXXX.eu-4.platformsh.site/
+   *         ^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^ ^^^^^^^^^^^^^
+   *
+   * It contains different parts:
+   *   1. Original domain
+   *   2. Platform environment (PLATFORM_ENVIRONMENT)
+   *   3. Platform project (PLATFORM_PROJECT)
+   */
+  if (!empty($_ENV['PLATFORM_ENVIRONMENT']) && !empty($_ENV['PLATFORM_PROJECT'])) {
+    $sites[$domain . '.' . $_ENV['PLATFORM_ENVIRONMENT'] . '-' . $_ENV['PLATFORM_PROJECT'] . '.eu-4.platformsh.site'] = $folder;
+  }
+}
