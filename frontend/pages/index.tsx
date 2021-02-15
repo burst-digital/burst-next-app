02549f313f42f6979fe96f44972d94690c161b5f
Jori Regter <joriregter@gmail.com>
2021-02-15 17:43:15 +0100

    adds styled components and corresponding components folder

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { H1 } from '../components/layout/Heading/Component';
 
 export default function Homepage() {
-  return <h1>Homepage</h1>;
+  return <H1>Homepage</H1>;
 }
