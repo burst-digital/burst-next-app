0bfec3b06333de194ce430a91acdf0ef523dc0c0
Jori Regter <joriregter@gmail.com>
2021-02-16 15:51:31 +0100

    adds i18n routing and getters

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   poweredByHeader: false,
+  i18n: {
+    locales: [
+      // INTL
+      'en-INTL',
+      'fr-INTL',
+      'nl-INTL',
+      // NL
+      'nl-NL',
+      // BE
+      'nl-BE',
+      'fr-BE',
+    ],
+    defaultLocale: 'en-INTL',
+    domains: [
+      {
+        domain: 'eurofiber.com',
+        defaultLocale: 'en-INTL',
+      },
+      {
+        domain: 'eurofiber.com.localhost',
+        defaultLocale: 'en-INTL',
+      },
+    ],
+  },
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { H1 } from '@components/layout/Heading/Component';
+import {
+  useLocaleIdentifier,
+  useRegionalLocaleIdentifiers,
+} from '@misc/locale-identifiers';
 
 export default function Homepage() {
-  return <H1>Homepage</H1>;
+  const localeIdentifier = useLocaleIdentifier();
+  const localeIdentifiersByRegion = useRegionalLocaleIdentifiers();
+
+  return (
+    <>
+      <H1>Homepage</H1>
+      <h2>Current locale</h2>
+      <pre>{JSON.stringify(localeIdentifier, null, 4)}</pre>
+
+      <h2>Locales</h2>
+      <pre>{JSON.stringify(localeIdentifiersByRegion, null, 4)}</pre>
+    </>
+  );
 }
