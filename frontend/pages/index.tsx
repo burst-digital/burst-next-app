1f4a708c567b417049fd183241837daf5b2b417b
Jori Regter <joriregter@gmail.com>
2021-02-16 15:50:31 +0100

    adds aliasing support to front-end

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { H1 } from '../components/layout/Heading/Component';
+import { H1 } from '@components/layout/Heading/Component';
 
 export default function Homepage() {
   return <H1>Homepage</H1>;
